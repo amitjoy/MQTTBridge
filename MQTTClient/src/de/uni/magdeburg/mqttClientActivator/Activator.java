70207ed0b525a90e7baa24a516d9024fbbfb22d3
Amit Kumar Mondal <admin@amitinside.com>
2017-06-26 21:41:44 +0200

    First version
    Signed-off-by: Amit Kumar Mondal <admin@amitinside.com>

--- MQTTClient/src/de/uni/magdeburg/mqttClientActivator/Activator.java
+++ MQTTClient/src/de/uni/magdeburg/mqttClientActivator/Activator.java
@@ -1,49 +1,61 @@
 package de.uni.magdeburg.mqttClientActivator;
 
-import org.osgi.framework.BundleActivator;
-import org.osgi.framework.BundleContext;
+import java.util.UUID;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
-import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
 
+public class Activator implements BundleActivator, MqttCallback {
 
-public class Activator implements BundleActivator {
-		
-	public void start(BundleContext Context) throws Exception {
-		System.out.println("Sample bundle starting");
-		MqttClient mqttClient = null;
-		
-		try {mqttClient = new MqttClient (
-					"ifatnode.et.uni-magedburg.de",
-					"Devang/Thesis/testData");
-		
-			 MqttConnectOptions connOpts = new MqttConnectOptions();
-			    connOpts.setCleanSession(true);			// clean the message session once disconnected
-			    mqttClient.connect(connOpts);
-			    System.out.println("Connected");}
-		
-		catch (MqttException me){
-		}
-	}
-	
-	public void connectionLost(Throwable cause) {
-	    // TODO Auto-generated method stub
+    private MqttClient mqttClient;
 
-	}
+    private static final String broker = "m2m.eclipse.org";
+    private static final String protocol = "tcp";
+    private static final int port = 1883;
+    private static final String clientId = UUID.randomUUID().toString();
 
-	public void messageArrived(String topic, MqttMessage message)
-	        throws Exception {
-	 System.out.println(message);   
-	 System.out.println(11);
-	}
-	
-	public void deliveryComplete(IMqttDeliveryToken token) {
-		 System.out.println(1);
-	}
-	
-	public void stop(BundleContext Context) throws Exception {
-		System.out.println("Sample bundle stopping");
-	}
+    @Override
+    public void start(final BundleContext Context) throws Exception {
+        try {
+            final String url = protocol + broker + ":" + port;
+            mqttClient = new MqttClient(url, clientId);
+
+            final MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setCleanSession(true);
+            connOpts.setConnectionTimeout(60);
+            connOpts.setKeepAliveInterval(60);
+            mqttClient.connect(connOpts);
+            mqttClient.setCallback(this);
+            mqttClient.subscribe("Java/Paho/OSGi");
+        } catch (final MqttException me) {
+            me.printStackTrace();
+        }
+    }
+
+    @Override
+    public void connectionLost(final Throwable cause) {
+        // no need
+    }
+
+    @Override
+    public void messageArrived(final String topic, final MqttMessage message) throws Exception {
+        System.out.println(message);
+    }
+
+    @Override
+    public void deliveryComplete(final IMqttDeliveryToken token) {
+    }
+
+    @Override
+    public void stop(final BundleContext Context) throws Exception {
+        if (mqttClient != null) {
+            mqttClient.close();
+        }
+    }
 }
